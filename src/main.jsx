d220172cedbf8f5b2b4712afda46386e9788b71a
study bro <studybro596@gmail.com>
2026-02-04 18:14:01 +0530

    fix base path and router for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { AdminProvider } from "./context/AdminContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <AuthProvider>
+  <HashRouter>
+<AuthProvider>
   <AdminProvider>
     <App />
   </AdminProvider>
 </AuthProvider>
+  </HashRouter>
+    
 
 );
